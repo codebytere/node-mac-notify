1d1732684915eb710d90b15e169295bdc4a01a18
Shelley Vohr <shelley.vohr@gmail.com>
2023-08-24 15:40:07 +0200

    test: more of them

--- test/module.spec.js
+++ test/module.spec.js
@@ -8,12 +8,60 @@ describe('node-mac-notify', () => {
     if (key) {
       const success = listener.remove(key)
       expect(success).to.be.true
+      key = ''
     }
   })
 
   it('should send a notification', () => {
     const success = postNotification('com.apple.some.test.key')
     expect(success).to.be.true
+  })
+
+  it('can suspend a notification', () => {
+    key = 'com.apple.some.test.key'
+
+    const added = listener.add(key)
+    expect(added).to.be.true
+
+    const suspended = listener.suspend(key)
+    expect(suspended).to.be.true
+  })
+
+  it('should throw if a non-existent notification is suspended', () => {
+    const failKey = 'com.apple.some.test.key'
+    expect(() => {
+      listener.suspend(failKey)
+    }).to.throw(`No observer exists for ${failKey}`)
+  })
+
+  it('can resume a suspended a notification', () => {
+    key = 'com.apple.some.test.key'
+
+    const added = listener.add(key)
+    expect(added).to.be.true
+
+    const suspended = listener.suspend(key)
+    expect(suspended).to.be.true
+
+    const resumed = listener.resume(key)
+    expect(resumed).to.be.true
+  })
+
+  it('should throw if a non-existent notification is resumed', () => {
+    const failKey = 'com.apple.some.test.key'
+    expect(() => {
+      listener.resume(failKey)
+    }).to.throw(`No observer exists for ${failKey}`)
+  })
+
+  it('can remove a notification', () => {
+    const removedKey = 'com.apple.some.test.key'
+
+    const added = listener.add(removedKey)
+    expect(added).to.be.true
+
+    const removed = listener.remove(removedKey)
+    expect(removed).to.be.true
   })
 
   it('should throw if a non-existent notification is removed', () => {
